76bc969e42b6d56ca0291584919e6d93b6690ccf
Yohay Halevi <yohaihalevi@gmail.com>
2026-02-10 10:37:05 +0200

    add count_ip_calls function

--- analyzer.py
+++ analyzer.py
@@ -1,16 +1,18 @@
-from practice.log_analyzer.reader import load_csv
+from config import *
+from reader import load_csv
 
 
 def extract_external_ip(data):
     return [line[1] for line in data if not line[1].startswith("192.168") and not line[1].startswith("10")]
 
 def filter_by_port(data):
-    return [line for line in data if line[3] == "22" or line[3] == "23" or line[3] == "3389"]
+    return [line for line in data if line[3] in SENSITIVE_PORTS]
 
 def filter_by_size(data):
-    return [line for line in data if int(line[5]) > 5000]
+    return [line for line in data if int(line[5]) > SIZE]
 
 def tag_traffic(data):
-    return [line + ["LARGE"] if int(line[5]) > 5000 else line + ["NORMAL"] for line in data]
+    return [line + ["LARGE"] if int(line[5]) > SIZE else line + ["NORMAL"] for line in data]
 
-print(tag_traffic(load_csv("network_traffic.log")))
+def count_ip_calls(data):
+    return {line_i[1]:sum([1 for line_j in data if line_j[1] == line_i[1]]) for index, line_i in enumerate(data) if line_i[1] not in [line_j[1] for line_j in data[:index]]}
