6987b52eddd7ad001446603d73865bf21765d14c
Yohay Halevi <yohaihalevi@gmail.com>
2026-02-10 10:37:26 +0200

    add map_port_to_protocol function

--- analyzer.py
+++ analyzer.py
@@ -16,3 +16,6 @@ def tag_traffic(data):
 
 def count_ip_calls(data):
     return {line_i[1]:sum([1 for line_j in data if line_j[1] == line_i[1]]) for index, line_i in enumerate(data) if line_i[1] not in [line_j[1] for line_j in data[:index]]}
+
+def map_port_to_protocol(data):
+    return {line[3]:line[4] for line in data}
