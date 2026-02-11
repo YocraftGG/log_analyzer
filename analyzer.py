9ab47fea4b9100ed1f6fd31884dbc7f85d10b49a
Yohay Halevi <yohaihalevi@gmail.com>
2026-02-11 10:23:55 +0200

    add filter_night_activity function

--- analyzer.py
+++ analyzer.py
@@ -22,6 +22,12 @@ def count_ip_calls(data):
 def map_port_to_protocol(data):
     return {line[3]:line[4] for line in data}
 
+def is_night_activity(hour):
+    if NIGHT_ACTIVITY[0] < NIGHT_ACTIVITY[1]:
+        return NIGHT_ACTIVITY[0] <= hour < NIGHT_ACTIVITY[1]
+    else:
+        return NIGHT_ACTIVITY[0] <= hour < 0 or hour < NIGHT_ACTIVITY[1]
+
 def suspicions(line):
     lst = []
     if not (line[1].startswith("192.168") or line[1].startswith("10")):
@@ -30,13 +36,9 @@ def suspicions(line):
         lst.append("PORT_SENSITIVE")
     if int(line[5]) > SIZE:
         lst.append("PACKET_LARGE")
-    hour = int(line[0].split(" ")[1].split(":")[0])
-    if NIGHT_ACTIVITY[0] < NIGHT_ACTIVITY[1]:
-        if NIGHT_ACTIVITY[0] <= hour < NIGHT_ACTIVITY[1]:
-            lst.append("ACTIVITY_NIGHT")
-    else:
-        if NIGHT_ACTIVITY[0] <= hour < 0 or hour < NIGHT_ACTIVITY[1]:
-            lst.append("ACTIVITY_NIGHT")
+    if is_night_activity(int(line[0].split(" ")[1].split(":")[0])):
+        lst.append("ACTIVITY_NIGHT")
+
     return lst
 
 def identifying_suspicions(data):
@@ -53,3 +55,6 @@ def bytes_to_kilobytes(bytes_):
 
 def filter_by_port_map(data):
     return list(filter(lambda line: line[3] in SENSITIVE_PORTS, data))
+
+def filter_night_activity(data):
+    return list(filter(lambda line: is_night_activity(int(line[0].split(" ")[1].split(":")[0])), data))
